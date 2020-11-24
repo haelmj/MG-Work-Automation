a1e2815f035f0b0dacb4260054b16e5b2341679e
Michael Ndon <manj19.mjn@gmail.com>
2020-11-24 11:02:13 +0100

    Added class methods for retrieving rows and their content

--- ReceiveStock/stock.py
+++ ReceiveStock/stock.py
@@ -17,6 +17,7 @@ class ReceiveStock():
         self.driver.get('https://mcndealers.com/users/sign_in')
         self.stock_window = self.driver.window_handles[0]
     def login(self):
+        """Handles Website login"""
         username = self.driver.find_element_by_id('login_name')
         username.clear()
         username.send_keys(LOGIN_NAME)
@@ -27,3 +28,29 @@ class ReceiveStock():
 
         enter = self.driver.find_element_by_name('login_btn')
         enter.send_keys(Keys.RETURN)
+    
+    def getRows(self, element):
+        row_group = element.find_element_by_tag_name('tbody')
+        rows = row_group.find_elements_by_tag_name('tr')
+        return rows
+    
+    def rowContent(self, tr):
+        row_list = tr.find_elements_by_tag_name('td')
+        row_content = [td.get_attribute('textContent') for td in row_list]
+        return row_content
+
+    def find_issued_stock(self):
+        """Navigate to Request Stock Tab, and find stock columns that are issued"""
+        self.login()
+        element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.LINK_TEXT, 'Request Stock'))).click()
+        time.sleep(5)
+        rows = self.getRows(element)
+        
+        for tr in rows:
+            view = tr.find_element_by_tag_name('a')
+            view_link =view.get_attribute('href')
+            
+            row_content = self.rowContent(tr)
+            row_content.pop()
+
